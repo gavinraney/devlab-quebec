2928a8eebda7200172b5f4df272c7f61da224752
Raney <graney1@una.edu>
2022-10-06 12:59:35 -0500

    catching up

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 console.log('in app.js')
 
+require("dotenv").config();
 const express = require('express')
 const path = require('node:path')
 const bodyParser = require('body-parser')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 const app = express()
 
@@ -10,28 +14,33 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs');
 
-
-let userName = "Joelzzzzz"; 
-let thatData = ""; 
-
 app.get('/', function (req, res) {
     // res.sendFile(path.join(__dirname, "index.html" )); 
     // res.send('Hello ' + userName + ' from Node/Express/Heroku');
-    // res.send(`Hello ${userName} from Node/Express/Heroku with Backticks!`)
 
-    //res.render('index', {userName: userName}      );
-})
+    client.connect(err => {
+        const collection = client.db("test").collection("devices");
+        console.log('connected!');
+        // perform actions on the collection object
+       
+        console.log('closed!');
+
+    
+        const result = collection.find().toArray();
+         console.log(result.title);
+            
+            res.send(result.title);
+            // client.close();
+        
+    });
+
+        // res.send(`Hello Express from inside my client connect f/n!`); 
+});
+
+    
+
+    // res.render('index',  {     }    );
+
 
 app.listen(process.env.PORT || 3000,
-  () => console.log("Server is running..."));
-
-//so much mongodb progress to catch up on
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://graney1:yesesd9@quebec.bzxlvoo.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-  console.log('closed!');
-});
+  () => console.log(`server is running on port: ${process.env.PORT}` ));
