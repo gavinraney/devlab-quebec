642bddfcdd121d4bd4e85edd952cab96c545dd4d
Esther Giles <47467263+esthergiles@users.noreply.github.com>
2022-10-10 14:52:37 -0500

    updates to view

--- public/main.js
+++ public/main.js
@@ -7,8 +7,8 @@ update.addEventListener('click', _ => {
     method: 'put',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      hamsterName: 'Hank',
-      hamsterAttribute: 'neat'
+      hamster: 'Hank',
+      attribute: 'neat'
     })
   })
     .then(res => {
