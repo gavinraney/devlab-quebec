cde728ac95e42238e7c8b6466397067457bf848c
Gavin Raney <graney1@una.edu>
2022-10-11 07:10:42 -0500

    take #100000001

--- server.js
+++ server.js
@@ -84,4 +84,4 @@ const PORT = process.env.PORT || 3000
 
 })  
 .catch(error => console.error(error))
-app.listen(PORT, console.log(`server is running on port: ${PORT}` ));
+//app.listen(PORT, console.log(`server is running on port: ${PORT}` ));
