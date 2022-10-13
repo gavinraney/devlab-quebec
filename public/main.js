74abd7184291c4b991e66c3bcb4ce024edc8532f
Raney <graney1@una.edu>
2022-10-13 13:02:06 -0500

    hank is gone

--- public/main.js
+++ public/main.js
@@ -24,7 +24,7 @@ deleteButton.addEventListener('click', _ => {
       method: 'delete',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        hamsterName: 'Hank'
+        hamsterName: 'michael'
       })
     })
       .then(res => {
